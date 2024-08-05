590359790fdba3eaaa1b1d463cf74ca738ed75d6
Luckas <luckasranarison@gmail.com>
2024-08-05 23:05:03 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      gray: "#1f2937",
+      primary: "#975f59",
+    },
+  },
+  plugins: [],
+};
